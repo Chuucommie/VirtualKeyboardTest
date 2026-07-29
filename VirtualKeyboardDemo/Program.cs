462358da12e66e130d261f315da80229e03820f5
IgelDev <igeldev@igelbeer.com>
2026-07-29 09:00:50 +0000

    feat: migrate to Fluent UI v5 components
    - Microsoft.FluentUI.AspNetCore.Components 5.0.0-rc.4
    - FluentTextInput replaces HTML input fields
    - FluentButton with ButtonAppearance for clear + show keyboard buttons
    - FluentCard + FluentStack for layout
    - Program.cs: AddFluentUIComponents() registered
    - App.razor: FluentTooltipProvider + FluentDialogProvider
    - index.html: Fluent UI CSS + JS module loaded

--- VirtualKeyboardDemo/Program.cs
+++ VirtualKeyboardDemo/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 using VirtualKeyboardDemo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Fluent UI services
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
